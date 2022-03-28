c677e94e2e799bc44fc030dd95ec912db4b8888d
Anish Pawar <56557214+AnishPawar@users.noreply.github.com>
2022-03-28 15:32:21 +0530

    [Upload and Download]

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,8 +1,18 @@
+from os import mkdir
 from flask import Flask, request, render_template, jsonify, send_file
 from flask_cors import CORS
+import datetime,time
+from anonymize import OCR,fuzzy_matching,NLP,batchAnonymize
 
 app = Flask(__name__)
 CORS(app)
+
+final_coor = []
+temp_list = []
+new_text = []
+image = []
+
+
 
 
 @app.route('/', methods= ['GET', 'POST'])
@@ -13,9 +23,9 @@ def lander():
         if request.form.getlist('foox'):
                 indexVal= request.form.getlist('foox')
                 print(indexVal)
-                send_file('receivedImages/WhatsApp Image 2022-03-19 at 7.39.54 PM.jpeg')
+                # send_file('receivedImages/WhatsApp Image 2022-03-19 at 7.39.54 PM.jpeg')
 
-    return render_template("index.html")
+    return render_template("index.html",path = '')
 
 @app.route('/upload_static_file', methods=['POST'])
 def upload_static_file():
@@ -23,18 +33,27 @@ def upload_static_file():
     print("Got request in static files")
     print(request.files)
     f = request.files['static_file']
-    f.save('receivedImages/'+f.filename)
+    x = datetime.datetime.now()
+    mkdir(f'static/receivedImages/{x}')
 
-    filePath = 'receivedImages/'+f.filename
+    # mkdir()
 
+    f.save(f'static/receivedImages/{x}/'+f.filename)
+
+    # print("OK")
+    path = batchAnonymize(f'static/receivedImages/{x}',ocr)
+    print("OK1")
+    print(path)
+    
     resp = {"success": True, "response": "file saved!"}
     # return jsonify(resp), 200
 
     # x = pd.read_csv(filePath)
     # dates = x['Date'].values.tolist()
     # closed = x['Volume'].values.tolist()
-    return render_template("index.html")
+    return render_template("index.html",path = path)
 
 
 if __name__ == "__main__":
+    ocr = OCR()
     app.run()
