b255757b2ce87b817872963063a4f7012c1bd2de
Akshat Ranka <akshatranka1423@gmail.com>
2022-04-02 21:54:22 +0530

    3rd commit

---
+++ WebApp/anonymize.py
@@ -0,0 +1,181 @@
+import nltk
+import os
+from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
+
+from fuzzywuzzy import fuzz as fz
+from fuzzywuzzy import process as pr
+import csv
+import re
+
+import cv2
+import numpy as np
+
+import easyocr
+
+class OCR:
+    def __init__(self) -> None:
+        self.reader = easyocr.Reader(['en'])
+    
+    def detectText(self,image):
+        self.image  = image
+        self.ocrResult = self.reader.readtext(self.image)
+        return self.ocrResult
+
+    def drawBoundingBox(self):
+        for i in self.ocrResult:
+            self.bbImage = cv2.rectangle(self.image, i[0][0], i[0][2], (0,255,0), 5)
+        return self.bbImage
+
+
+
+def NLP(text):
+    # global new_text
+    # Text Pre-Processing
+    text = text.split(' ')
+
+    new = []
+
+    for i in text:
+        i = i.strip()
+        
+        a = i.replace('\n','')
+        a2 = "".join(re.findall("[a-zA-Z]+", a))
+
+        if len(i) > 2:
+            new.append(a2.capitalize())
+
+    # print(new)
+    processed = " ".join(new)
+
+    ne_tree = pos_tag(word_tokenize(processed))
+    # print(ne_tree)
+    names = []
+
+    # Checking P-Noun Followed by P-Noun
+    for c in range(len(ne_tree)):
+
+        if ((ne_tree[c-1][1] == "NNP") and (ne_tree[c][1] == "NNP")) or ((ne_tree[c-1][1] == "NNP") and (ne_tree[c][1] == "NN")) or ((ne_tree[c-1][1] == "NN") and (ne_tree[c][1] == "NNP")):
+            
+            names.append(ne_tree[c-1][0])
+            names.append(ne_tree[c][0])
+    
+    # If list is empty, return the original text
+    if not names :
+        for c in ne_tree:
+            temp = "".join(re.findall("[a-zA-Z]+", c[0].lower()))
+            # new_text.append(temp)
+        return text
+    
+    
+    else:
+        # temp_list = names
+        return set(names)
+
+def fuzzy_matching(text):
+    # global new_text
+    # print(text)
+    #Declarations
+    new_text = []
+
+    # if blur_stat == 0:
+
+    Fname_ds = []
+    Lname_ds = []
+    body_part = ['abdomen', 'barium', 'bone', 'chest', 'dental', 'extremity', 'hand', 'joint', 'neck', 'pelvis', 'sinus', 'skull', 'spine', 'thoracic']
+
+    print(text)
+
+    #Opening CSV files
+    with open('dependencies/Indian_Names.csv', newline='') as f:
+        reader = csv.reader(f)
+        next(reader)
+        for e in reader:
+            # print(e)
+            Fname_ds.append(e[1])
+
+    with open('dependencies/indian_last_name.csv', newline='') as f:
+        reader = csv.reader(f)
+        next(reader)
+
+        for e in reader:
+            Lname_ds.append(e[0])
+
+
+    if len(text) < 3 or pr.extractOne(text, body_part)[1] > 75: 
+        return
+
+    #Checking for first name
+    for given in Fname_ds:
+        if fz.ratio(text, given) > 60:
+            # print(text, given, 1)
+            new_text.append(text)
+    
+    #Checking for last name
+    for given in Lname_ds:
+        if fz.ratio(text, given) > 60:
+            # print(text, given, 2)
+            new_text.append(text)
+    
+    return new_text
+
+
+def batchAnonymize(imgDir,ocr):
+    # dstImg = f'{imgDir}/'
+    print("IT")
+    print(imgDir)
+    # os.mkdir(f'{imgDir}/output')
+
+    for imgSingle in os.listdir(imgDir):
+        # print(f'{imgDir}/{i}')
+
+        if imgSingle!=".DS_Store":
+
+            img = cv2.imread(f'{imgDir}{imgSingle}')
+
+            print(f'{imgDir}/{imgSingle}')
+
+            dat = ocr.detectText(img)
+            
+            textstr = ''
+            for i in dat:
+                textstr+= f'{i[1]} '
+            
+            textNLP = NLP(textstr)
+            retlist = []
+            for i in textNLP:
+                retlist.append(fuzzy_matching(i))
+
+            unique  = []
+            for i in retlist:
+                if i:
+                    unique.append(i[0])
+            
+            set(unique)
+            dataDict = {'coords':[],'text':[]}
+
+            for i in dat:
+                # print(i)
+                dataDict['coords'].append(i[0])
+                dataDict['text'].append(i[1])
+            
+            coorList = []
+
+
+            for i in set(unique):
+                # print(i.lower())
+                for j in range(len(dat)):
+                    text = dat[j][1].split(' ')
+                    for word in text:
+                        if i.lower() == word.lower():
+                            
+                            coorList.append(dat[j][0])
+            print("I Isss")
+            print(imgSingle)
+            for coor in coorList:
+                img = cv2.rectangle(img, coor[0], coor[2], (0,255,0), -1)
+            print("I Isss111")
+            print(imgSingle)
+            cv2.imwrite(f'static/outputs/{imgSingle}',img)
+    return f'{imgDir[7:]}/x{imgSingle}'
+    
--- WebApp/app.py
+++ WebApp/app.py
@@ -1,8 +1,20 @@
+from zipfile import ZipFile
+import os
+from os import mkdir
 from flask import Flask, request, render_template, jsonify, send_file
 from flask_cors import CORS
+import datetime,time
+from anonymize import OCR,fuzzy_matching,NLP,batchAnonymize
 
 app = Flask(__name__)
 CORS(app)
+
+final_coor = []
+temp_list = []
+new_text = []
+image = []
+
+
 
 
 @app.route('/', methods= ['GET', 'POST'])
@@ -13,77 +25,58 @@ def lander():
         if request.form.getlist('foox'):
                 indexVal= request.form.getlist('foox')
                 print(indexVal)
-                send_file('receivedImages/WhatsApp Image 2022-03-19 at 7.39.54 PM.jpeg')
+                # send_file('receivedImages/WhatsApp Image 2022-03-19 at 7.39.54 PM.jpeg')
 
-    return render_template("index.html")
+    return render_template("index.html",path = '0')
 
 @app.route('/upload_static_file', methods=['POST'])
 def upload_static_file():
     global filePath
     print("Got request in static files")
     print(request.files)
-    f = request.files['static_file']
-    f.save('receivedImages/'+f.filename)
 
-    filePath = 'receivedImages/'+f.filename
+    files = request.files.getlist('static_file')
 
+    for f in files:
+        f.save(f'static/receivedImages/'+f.filename)
+    # f = request.files['static_file']
+
+    
+    # x = datetime.datetime.now()
+    # mkdir(f'static/receivedImages/{x}')
+
+    # mkdir()
+
+    
+
+    # print("OK")
+    batchAnonymize(f'static/receivedImages/',ocr)
+    print("OK1")
+    
+    zipObj = ZipFile('static/outputImages.zip', 'w')
+    for i in os.listdir('static/outputs'):
+        zipObj.write(f'static/outputs/{i}')
+
+
+
+    # print(path)
+    
     resp = {"success": True, "response": "file saved!"}
     # return jsonify(resp), 200
 
     # x = pd.read_csv(filePath)
     # dates = x['Date'].values.tolist()
     # closed = x['Volume'].values.tolist()
-    return render_template("index.html")
+    return render_template("index.html",path = '1')
 
-# Akshat 
+
 if __name__ == "__main__":
+    ocr = OCR()
+
+    for i in os.listdir('static/outputs'):
+        os.remove(f'static/outputs/{i}')
+    
+    for i in os.listdir('static/receivedImages'):
+        os.remove(f'static/receivedImages/{i}')
+
     app.run()
-
-
-@app.route('/uploads/<filename>')
-def upload(filename):
-    return send_file(app.config['UPLOAD_PATH'], filename)
-
-@app.route('/uploads/<filename>')
-def upload(filename):
-    return send_file(app.config['UPLOAD_PATH'], filename)
-
-# import imghdr
-# import os
-# from flask import Flask, render_template, request, redirect, url_for, abort, \
-#     send_from_directory
-# from werkzeug.utils import secure_filename
-
-# app = Flask(__name__)
-# app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
-# app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
-# app.config['UPLOAD_PATH'] = 'uploads'
-
-# def validate_image(stream):
-#     header = stream.read(512)  # 512 bytes should be enough for a header check
-#     stream.seek(0)  # reset stream pointer
-#     format = imghdr.what(None, header)
-#     if not format:
-#         return None
-#     return '.' + (format if format != 'jpeg' else 'jpg')
-
-# @app.route('/')
-# def index():
-#     files = os.listdir(app.config['UPLOAD_PATH'])
-#     return render_template('index.html', files=files)
-
-# @app.route('/', methods=['POST'])
-# def upload_files():
-#     uploaded_file = request.files['file']
-#     filename = secure_filename(uploaded_file.filename)
-#     if filename != '':
-#         file_ext = os.path.splitext(filename)[1]
-#         if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
-#                 file_ext != validate_image(uploaded_file.stream):
-#             abort(400)
-#         uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-#     return redirect(url_for('index'))
-
-# @app.route('/uploads/<filename>')
-# def upload(filename):
-#     return send_from_directory(app.config['UPLOAD_PATH'], filename)
